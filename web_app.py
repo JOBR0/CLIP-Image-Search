656fa9ad7c328be837e2e5132b4df2ff6fda2bc6
JOBR0 <45601045+JOBR0@users.noreply.github.com>
2022-02-24 06:07:28 +0100

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -30,7 +30,8 @@ def init_app():
     cache = diskcache.Cache("./cache")
     long_callback_manager = DiskcacheLongCallbackManager(cache)
     #app = dash.Dash(__name__, server=server, long_callback_manager=long_callback_manager)
-    app = dash.Dash(__name__, long_callback_manager=long_callback_manager)
+    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+    app = dash.Dash(__name__, long_callback_manager=long_callback_manager, external_stylesheets=external_stylesheets)
 
 
     device = "cpu"
@@ -60,74 +61,68 @@ def init_app():
 
     static_image_route = "/static/"
 
-    #img1 = html.Img(src='data:image/png;base64,{}'.format(encoded_image))
-    #img1 = html.Img(src=static_image_route + "test2.png")
-    #img2 = html.Img(src=static_image_route + "test.JPG")
+    upload = dcc.Upload(
+        id='upload-image',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    )
+
+
 
 
     store = dcc.Store(id="image-paths")
 
-    app.layout = html.Div(children=[title_div, text_input, label1, button, content_div, store])
+    app.layout = html.Div(children=[title_div, text_input, label1, button, upload, content_div, store])
 
     # Add a static image route that serves images from desktop
     # Be *very* careful here - you don't want to serve arbitrary files
     # from your computer or server
-    @app.server.route(f"{static_image_route}<image_path>")
-    def serve_image(image_path):
-        #image_name = '{}.png'.format(image_path)
+    #@app.server.route(f"{static_image_route}<image_path>")
+    @app.server.route(f"{static_image_route}<path:filename>.<ext>")
+    def serve_image(filename, ext):
+        print(f"serving {filename}.{ext}")
         # if image_name not in list_of_images:
         #     raise Exception('"{}" is excluded from the allowed static files'.format(image_path))
-        return flask.send_from_directory("", image_path)
 
-    # @app.callback(Output("output", "children"), Input("input", "value"))
-    # def update_output(input):
-    #     if not input:
-    #         raise PreventUpdate
-    #
-    #     try:
-    #         img = np.array(Image.open("test.jpg"))
-    #     except OSError:
-    #         raise PreventUpdate
-    #
-    #     fig = px.imshow(img, color_continuous_scale="gray")
-    #     fig.update_layout(coloraxis_showscale=False)
-    #     fig.update_xaxes(showticklabels=False)
-    #     fig.update_yaxes(showticklabels=False)
-    #
-    #     return dcc.Graph(figure=fig)
+        return flask.send_from_directory("//nas_enbaer", f"{filename}.{ext}")
+        #return flask.send_from_directory("", image_path)
 
-    # @app.long_callback(
-    #     output=Output("content-div", "children"),
-    #     inputs=Input("button", "n_clicks"),
-    #     state=State(component_id="input-box", component_property="value"),
-    #     running=[(Output("button", "disabled"), True, False)],
-    #     progress=Output("label1", "children"),
-    #     manager=long_callback_manager,
-    # )
     def callback(set_progress, n_clicks, input_value, model=model, device=device, image_features=image_features, paths=paths):
         print("callback")
         print(n_clicks)
+        if n_clicks is None:
+            return []
         print(input_value)
-        imgs = []
-        for i in range(10):
-            img = html.Img(src=static_image_route + "test.JPG", style={"height": "500px"})
-            # img = Image.open("test.JPG")
-            # img = ImageOps.exif_transpose(img)
-            # img = np.array(img)
+        print(input_value)
+        text_features = encode_text(input_value, model, device)
 
-            # # img = np.array(Image.open("test.JPG"))
-            # fig = px.imshow(img, color_continuous_scale="gray")
-            # fig.update_layout(coloraxis_showscale=False)
-            # fig.update_xaxes(showticklabels=False)
-            # fig.update_yaxes(showticklabels=False)
-            # g = dcc.Graph(figure=fig, config={'staticPlot': True})  # , style={'width': '30vh', 'height': '30vh'})
-            #g = "test"
+        print("start search")
+        top_img_files, top_values = search(query_features=text_features,
+                                           image_features=image_features,
+                                           paths=paths,
+                                           n_results=6)
+        print("end search")
+        imgs = []
+        for i, img_file in enumerate(top_img_files):
+            path = img_file[13:]
+
+            img = html.Img(src=static_image_route + path, style={"height": "500px"})
             imgs.append(img)
-            # set_progress(f"{i}/{len(top_img_files)}")
-            #            set_progress(graphs)
-            # children.append("{}".format(i))
-            # time.sleep(1)
-            set_progress(f"{i}/{len(imgs)}")
+            set_progress(f"{i}/{len(top_img_files)}")
 
         return imgs
 
